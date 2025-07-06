b08a7b1f3a4be1752f710cfbcceabe4dcf4885cc
Omkar Hole <omkarhole314@gmail.com>
2025-07-06 16:23:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -259,9 +259,6 @@ app.post("/sellOrder",async(req,res)=>{
 
 //chatgpt 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 app.listen(PORT,()=>{
